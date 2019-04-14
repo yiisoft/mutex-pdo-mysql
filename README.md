ce461972ce6a07e0b6e6976ac5352069be57ae99
Mehdi Achour <machour@gmail.com>
2019-04-14 19:57:16 +0100

    MySQL Mutex

---
+++ README.md
@@ -0,0 +1,16 @@
+<p align="center">
+    <a href="https://github.com/yiisoft" target="_blank">
+        <img src="https://avatars0.githubusercontent.com/u/993323" height="100px">
+    </a>
+    <h1 align="center">Yii Mutex Library - MySQL DB Driver</h1>
+    <br>
+</p>
+
+This library provides a MySQL mutex implementation for [yiisoft/mutex].
+
+[yiisoft/mutex]: https://github.com/yiisoft/mutex
+
+[![Latest Stable Version](https://poser.pugx.org/yiisoft/mutex-db-mysql/v/stable.png)](https://packagist.org/packages/yiisoft/mutex-db-mysql)
+[![Total Downloads](https://poser.pugx.org/yiisoft/mutex-db-mysql/downloads.png)](https://packagist.org/packages/yiisoft/mutex-db-mysql)
+[![Build Status](https://travis-ci.com/yiisoft/mutex-db-mysql.svg?branch=master)](https://travis-ci.com/yiisoft/mutex-db-mysql)
+
---
+++ src/MysqlMutex.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ *
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace Yii\Mutex;
+
+/**
+ * MysqlMutex implements mutex "lock" mechanism via MySQL locks.
+ *
+ * @see Mutex
+ */
+class MysqlMutex extends Mutex
+{
+    /**
+     * @var \PDO
+     */
+    protected $connection;
+
+    /**
+     * DbMutex constructor.
+     *
+     * @param \PDO $connection
+     * @param bool $autoRelease whether all locks acquired in this process (i.e. local locks) must be released
+     *                          automatically before finishing script execution. Defaults to true. Setting this property
+     *                          to true means that all locks acquired in this process must be released (regardless of
+     *                          errors or exceptions).
+     */
+    public function __construct(\PDO $connection, $autoRelease = true)
+    {
+        $this->connection = $connection;
+        $driverName = $connection->getAttribute(\PDO::ATTR_DRIVER_NAME);
+        if ($driverName !== 'mysql') {
+            throw new \InvalidArgumentException('MySQL connection instance should be passed. Got '.$driverName.'.');
+        }
+
+        parent::__construct($autoRelease);
+    }
+
+    /**
+     * Acquires lock by given name.
+     *
+     * @param string $name    of the lock to be acquired.
+     * @param int    $timeout time (in seconds) to wait for lock to become released.
+     *
+     * @return bool acquiring result.
+     *
+     * @see http://dev.mysql.com/doc/refman/5.0/en/miscellaneous-functions.html#function_get-lock
+     */
+    protected function acquireLock($name, $timeout = 0)
+    {
+        $statement = $this->connection->prepare('SELECT GET_LOCK(:name, :timeout)');
+        $statement->bindValue(':name', $this->hashLockName($name));
+        $statement->bindValue(':timeout', $timeout);
+        $statement->execute();
+
+        return $statement->fetchColumn();
+    }
+
+    /**
+     * Releases lock by given name.
+     *
+     * @param string $name of the lock to be released.
+     *
+     * @return bool release result.
+     *
+     * @see http://dev.mysql.com/doc/refman/5.0/en/miscellaneous-functions.html#function_release-lock
+     */
+    protected function releaseLock($name)
+    {
+        $statement = $this->connection->prepare('SELECT RELEASE_LOCK(:name)');
+        $statement->bindValue(':name', $this->hashLockName($name));
+        $statement->execute();
+
+        return $statement->fetchColumn();
+    }
+
+    /**
+     * Generate hash for lock name to avoid exceeding lock name length limit.
+     *
+     * @param string $name
+     *
+     * @return string
+     *
+     * @since 2.0.16
+     * @see https://github.com/yiisoft/yii2/pull/16836
+     */
+    protected function hashLockName($name)
+    {
+        return sha1($name);
+    }
+}
